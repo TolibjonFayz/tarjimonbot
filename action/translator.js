0163a324938ffe29cdf0198cb57eb2e3c2f8c177
Tolibjon <tolibjonfayz@gmail.com>
2024-07-03 11:30:04 +0500

    Translator api is changed

--- action/translator.js
+++ action/translator.js
@@ -1,38 +1,36 @@
 const { bot } = require("../core/bot");
 const UserLang = require("../db/Language");
-const translate = require("google-translate-api-x");
+// const translate = require("google-translate-api-x");
+const { translate } = require("bing-translate-api");
 
 bot.on("text", async (ctx) => {
   // // // Userni holati qaysi tildaligini bilamiz
   const userlang = await UserLang.findOne({ userid: ctx.from.id });
 
   // // // Kelayotgan matn qaysi tildaligini bilamiz
-  const textlang = await translate(ctx.message.text, {
-    from: "auto",
-    to: "en",
-  });
-  const detectedLanguage = textlang.from.language.iso;
+  const textlang = await translate(ctx.message.text, null, "en");
+  const detectedLanguage = textlang.language.from;
 
   // // // Lest translate
   // If user language is english
   if (userlang.lang == "english") {
     if (detectedLanguage == "uz") {
-      const result = await translate(ctx.message.text, { to: "en" });
-      ctx.reply(result.text);
+      const result = await translate(ctx.message.text, "uz", "en");
+      ctx.reply(result.translation);
     } else {
-      const result = await translate(ctx.message.text, { to: "uz" });
-      ctx.reply(result.text);
+      const result = await translate(ctx.message.text, null, "uz");
+      ctx.reply(result.translation);
     }
   }
 
   // If user language is russian
   if (userlang.lang == "russian") {
-    if (detectedLanguage == "uz") {
-      const result = await translate(ctx.message.text, { to: "ru" });
-      ctx.reply(result.text);
+    if (detectedLanguage == "ru") {
+      const result = await translate(ctx.message.text, "ru", "uz");
+      ctx.reply(result.translation);
     } else {
-      const result = await translate(ctx.message.text, { to: "uz" });
-      ctx.reply(result.text);
+      const result = await translate(ctx.message.text, null, "ru");
+      ctx.reply(result.translation);
     }
   }
 
@@ -43,8 +41,8 @@ bot.on("text", async (ctx) => {
         "O'zbek tilida emas boshqa tildagi matn kiriting yoki boshqa tilni tanlang 👉 /start"
       );
     } else {
-      const result = await translate(ctx.message.text, { to: "uz" });
-      ctx.reply(result.text);
+      const result = await translate(ctx.message.text, null, "uz");
+      ctx.reply(result.translation);
     }
   }
 });
